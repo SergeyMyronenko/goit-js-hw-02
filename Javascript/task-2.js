1b755667e3452199bcc3912b4525f01d555ab87c
Myronenlo <55333290+SergeyMyronenko@users.noreply.github.com>
2023-10-17 23:36:20 +0300

    update homework

--- Javascript/task-2.js
+++
@@ -1,7 +0,0 @@
-"use strict"
-
-function formatMessage(message, maxLength) { 
-    const changeMessage = message.slice(0, maxLength) + "...";
-    const result = message.length <= maxLength ? `"${message}"` : `"${changeMessage}"`;
-}
-
